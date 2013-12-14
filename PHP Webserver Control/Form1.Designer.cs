4e8142111b1c5e57507d949704d9b92fe643512e
Matthew Hazlett <hazlema@gmail.com>
2013-12-14 18:21:15 -0500

    Stable Build

---
+++ PHP Webserver Control/Form1.Designer.cs
@@ -0,0 +1,324 @@
+﻿namespace PHP_Webserver_Control
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbStatus = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.cbBrowser = new System.Windows.Forms.CheckBox();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.lbRoot = new System.Windows.Forms.ListBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.tmrStatus = new System.Windows.Forms.Timer(this.components);
+            this.dirBrowse = new System.Windows.Forms.FolderBrowserDialog();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.cbHide = new System.Windows.Forms.CheckBox();
+            this.tbPort = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbHost = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.tbLocation = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.tbPort)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(224, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "PHP Webserver Status:";
+            // 
+            // tbStatus
+            // 
+            this.tbStatus.AutoSize = true;
+            this.tbStatus.ForeColor = System.Drawing.Color.DarkRed;
+            this.tbStatus.Location = new System.Drawing.Point(350, 9);
+            this.tbStatus.Name = "tbStatus";
+            this.tbStatus.Size = new System.Drawing.Size(37, 13);
+            this.tbStatus.TabIndex = 1;
+            this.tbStatus.Text = "Offline";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.cbBrowser);
+            this.groupBox1.Controls.Add(this.btnRemove);
+            this.groupBox1.Controls.Add(this.btnAdd);
+            this.groupBox1.Controls.Add(this.lbRoot);
+            this.groupBox1.Location = new System.Drawing.Point(16, 187);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(371, 194);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Select Document Root";
+            // 
+            // cbBrowser
+            // 
+            this.cbBrowser.AutoSize = true;
+            this.cbBrowser.Checked = true;
+            this.cbBrowser.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.cbBrowser.Location = new System.Drawing.Point(9, 165);
+            this.cbBrowser.Name = "cbBrowser";
+            this.cbBrowser.Size = new System.Drawing.Size(115, 17);
+            this.cbBrowser.TabIndex = 3;
+            this.cbBrowser.Text = "Start Web Browser";
+            this.cbBrowser.UseVisualStyleBackColor = true;
+            this.cbBrowser.CheckedChanged += new System.EventHandler(this.SaveList);
+            this.cbBrowser.Click += new System.EventHandler(this.SaveList);
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.btnRemove.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnRemove.Location = new System.Drawing.Point(281, 159);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(75, 23);
+            this.btnRemove.TabIndex = 2;
+            this.btnRemove.Text = "&Remove";
+            this.btnRemove.UseVisualStyleBackColor = false;
+            this.btnRemove.Click += new System.EventHandler(this.RemoveFolder);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(200, 159);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "&Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.AddFolder);
+            // 
+            // lbRoot
+            // 
+            this.lbRoot.FormattingEnabled = true;
+            this.lbRoot.Location = new System.Drawing.Point(9, 19);
+            this.lbRoot.Name = "lbRoot";
+            this.lbRoot.Size = new System.Drawing.Size(347, 134);
+            this.lbRoot.Sorted = true;
+            this.lbRoot.TabIndex = 0;
+            this.lbRoot.SelectedIndexChanged += new System.EventHandler(this.ItemSelected);
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.Color.LightCoral;
+            this.btnStart.Enabled = false;
+            this.btnStart.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStart.Location = new System.Drawing.Point(25, 397);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(347, 23);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.Text = "&Start Server";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.RunServer);
+            // 
+            // tmrStatus
+            // 
+            this.tmrStatus.Enabled = true;
+            this.tmrStatus.Interval = 1000;
+            this.tmrStatus.Tick += new System.EventHandler(this.CheckStatus);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.cbHide);
+            this.groupBox2.Controls.Add(this.tbPort);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.tbHost);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.btnBrowse);
+            this.groupBox2.Controls.Add(this.tbLocation);
+            this.groupBox2.Location = new System.Drawing.Point(16, 29);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(371, 152);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "PHP Options";
+            // 
+            // cbHide
+            // 
+            this.cbHide.AutoSize = true;
+            this.cbHide.Checked = true;
+            this.cbHide.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.cbHide.Location = new System.Drawing.Point(9, 68);
+            this.cbHide.Name = "cbHide";
+            this.cbHide.Size = new System.Drawing.Size(115, 17);
+            this.cbHide.TabIndex = 8;
+            this.cbHide.Text = "Hide PHP Window";
+            this.cbHide.UseVisualStyleBackColor = true;
+            this.cbHide.CheckedChanged += new System.EventHandler(this.SaveList);
+            // 
+            // tbPort
+            // 
+            this.tbPort.Increment = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.tbPort.Location = new System.Drawing.Point(281, 116);
+            this.tbPort.Maximum = new decimal(new int[] {
+            9999,
+            0,
+            0,
+            0});
+            this.tbPort.Minimum = new decimal(new int[] {
+            80,
+            0,
+            0,
+            0});
+            this.tbPort.Name = "tbPort";
+            this.tbPort.Size = new System.Drawing.Size(75, 20);
+            this.tbPort.TabIndex = 7;
+            this.tbPort.ThousandsSeparator = true;
+            this.tbPort.Value = new decimal(new int[] {
+            80,
+            0,
+            0,
+            0});
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(281, 99);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Port:";
+            // 
+            // tbHost
+            // 
+            this.tbHost.Location = new System.Drawing.Point(9, 116);
+            this.tbHost.Name = "tbHost";
+            this.tbHost.Size = new System.Drawing.Size(266, 20);
+            this.tbHost.TabIndex = 5;
+            this.tbHost.Text = "localhost";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Server Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Location of php.exe";
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(281, 62);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 2;
+            this.btnBrowse.Text = "&Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.LocatePHP);
+            // 
+            // tbLocation
+            // 
+            this.tbLocation.Location = new System.Drawing.Point(6, 36);
+            this.tbLocation.Name = "tbLocation";
+            this.tbLocation.Size = new System.Drawing.Size(350, 20);
+            this.tbLocation.TabIndex = 0;
+            this.tbLocation.Text = "c:\\php\\php.exe";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.DefaultExt = "exe";
+            this.openFileDialog1.Filter = "EXE Files|*.exe";
+            this.openFileDialog1.Title = "Location of php.exe";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(403, 432);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.tbStatus);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PHP Webserver Control";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.SaveList);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.tbPort)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label tbStatus;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.ListBox lbRoot;
+        private System.Windows.Forms.Timer tmrStatus;
+        private System.Windows.Forms.FolderBrowserDialog dirBrowse;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.NumericUpDown tbPort;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbHost;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.TextBox tbLocation;
+        private System.Windows.Forms.CheckBox cbBrowser;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.CheckBox cbHide;
+    }
+}
+
---
+++ PHP Webserver Control/Form1.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using System.Collections.Generic;
+
+
+namespace PHP_Webserver_Control
+{
+    public partial class Form1 : Form
+    {
+
+        String Filename = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "PHPWebserverControl.xml");
+        
+        public Form1()
+        {
+            InitializeComponent();
+            LoadList();
+            CheckStatus(new Object(), new EventArgs());
+        }
+
+        /*
+         * Method: SaveList (And its overloads)
+         *   Desc: Saves all the items in the listbox
+         */
+        private void SaveList()
+        {
+            // Header and Options
+            //
+            XDocument saveList = new XDocument(
+                new XDeclaration("1.0", "utf-16", "true"),
+                new XComment("PHP Webserver Control Data File"),
+                new XElement("WebserverControl", 
+                    new XElement("serverOptions",
+                        new XElement("php", tbLocation.Text),
+                        new XElement("host", tbHost.Text),
+                        new XElement("port", tbPort.Value.ToString()),
+                        new XElement("browser", cbBrowser.Checked.ToString()),
+                        new XElement("hide", cbHide.Checked.ToString())
+                    )
+                )
+            );
+
+            // Items
+            //
+            XElement DocRoot = new XElement("serverRoot");
+
+            foreach (string item in lbRoot.Items)
+                DocRoot.Add(new XElement("path", item));
+
+            // Save
+            //
+            saveList.Element("WebserverControl").Add(DocRoot);
+            saveList.Save(Filename);
+        }
+        private void SaveList(object sender, FormClosingEventArgs e)
+        {
+            SaveList();
+        }
+        private void SaveList(object sender, EventArgs e)
+        {
+            SaveList();
+        }
+
+        /*
+         * Method: LoadList
+         *   Desc: Loads the XML into the listbox
+         */
+        private void LoadList()
+        {
+            if (File.Exists(Filename))
+            {
+                XDocument DocRoot = XDocument.Load(Filename);
+
+                // Load List
+                //
+                lbRoot.Items.AddRange(
+                    (from r in DocRoot.Elements("WebserverControl").Elements("serverRoot").Descendants()
+                     select r.Value).ToArray()
+                );
+
+                // Load settings into dictionary
+                //
+                Dictionary<string, string> CfgSettings = (from i in DocRoot.Elements("WebserverControl").Elements("serverOptions").Descendants()
+                                                          select (new { i.Name, i.Value }))
+                                                          .ToDictionary(i => i.Name.ToString(), i => i.Value);
+
+                // Populate Controls
+                //
+                tbLocation.Text   = CfgSettings["php"];
+                tbHost.Text       = CfgSettings["host"];
+                tbPort.Value      = Convert.ToInt16(   CfgSettings["port"] );
+                cbBrowser.Checked = Convert.ToBoolean( CfgSettings["browser"] );
+                cbHide.Checked    = Convert.ToBoolean( CfgSettings["hide"] );
+            }
+
+        }
+
+        /*
+         * Method: CheckStatus
+         *   Desc: Determins if php is running
+         */
+        private void CheckStatus(object sender, EventArgs e)
+        {
+            // Opdate Controls
+            //
+            tbStatus.ForeColor = Color.Red;
+            btnStart.BackColor = SystemColors.Control;
+            tbStatus.Text = "Offline";
+            btnStart.Text = "&Start Server";
+
+            if (this.cbBrowser.Checked)
+                btnStart.Text += " and Browser";
+
+            // PHP Running?
+            //
+            foreach (Process thisProcess in Process.GetProcesses())
+            {
+                if (thisProcess.ProcessName.Contains("php"))
+                {
+                    tbStatus.ForeColor = Color.Green;
+                    tbStatus.Text = "Active";
+                    btnStart.Text = "&Stop Server";
+                    btnStart.BackColor = Color.Red;
+                }
+            }
+        }
+
+        /*
+         * Method: RunServer
+         *   Desc: Start / Stop Server
+         */
+        private void RunServer(object sender, EventArgs e)
+        {
+           // Are we starting or stopping?
+           //
+           if (btnStart.Text.Contains("Start"))
+           {
+               if (File.Exists(tbLocation.Text))
+               {
+                   string cmd = tbLocation.Text;
+                   string arg = "-S " + tbHost.Text + ":" + tbPort.Value + " -t " + lbRoot.SelectedItem;
+
+                   ProcessStartInfo Start = new ProcessStartInfo(cmd, arg);
+                   Start.WindowStyle = ProcessWindowStyle.Minimized;
+
+                   if (cbHide.Checked)
+                       Start.WindowStyle = ProcessWindowStyle.Hidden;
+
+                   Process.Start(Start);
+
+                   if (cbBrowser.Checked)
+                       Process.Start("http://" + tbHost.Text + ":" + tbPort.Value);
+               }
+               else
+                   MessageBox.Show("Unable to find PHP.EXE", "Error");
+           }
+           else
+           {
+               // Kill all php processes
+               //
+               foreach (Process thisProcess in Process.GetProcesses())
+               {
+                   if (thisProcess.ProcessName.Contains("php"))
+                       thisProcess.Kill();
+               }
+           }
+        }
+
+        /*
+         * Method: AddFolder
+         *   Desc: Adds a folder to the LB then saves
+         */
+        private void AddFolder(object sender, EventArgs e)
+        {
+            if (dirBrowse.ShowDialog() == DialogResult.OK)
+                lbRoot.Items.Add(dirBrowse.SelectedPath.ToString());
+            SaveList();
+       }
+
+        /*
+         * Method: RemoveFolder
+         *   Desc: Removes a folder to the LB then saves
+         */
+        private void RemoveFolder(object sender, EventArgs e)
+        {
+            lbRoot.Items.RemoveAt(lbRoot.SelectedIndex);
+            SaveList();
+        }
+
+        /*
+         * Method: ItemSelected
+         *   Desc: Enable the start / stop button
+         */
+        private void ItemSelected(object sender, EventArgs e)
+        {
+            btnStart.Enabled = true;
+        }
+
+        /*
+         * Method: LocatePHP
+         *   Desc: Browse for PHP
+         */
+        private void LocatePHP(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                tbLocation.Text = openFileDialog1.FileName;
+        }
+    }
+}
